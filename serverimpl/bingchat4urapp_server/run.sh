879dc2bffea827a416588b460d66df109f5a7ce7
Egor Vityazev <55443477+VityazevEgor@users.noreply.github.com>
2024-11-01 23:09:14 +0300

    Craeted run.sh
    Created bash script to run server app

---
+++ serverimpl/bingchat4urapp_server/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd target
+java --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -jar bingchat4urapp_server-0.0.4-SNAPSHOT.jar --proxy 127.0.0.1:2080 --examMode true --hideBrowser false
